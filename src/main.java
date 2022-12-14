3683d5fdd22c682e9a21d21be36e15d0ddff1e53
Артем Кондратенко <artem-osteo@yandex.ru>
2022-12-14 14:41:26 +0300

    Add fael

---
+++ src/main.java
@@ -0,0 +1,17 @@
+public class main {
+    public static void main(String[] args) {
+        int initial_account = 100;
+        int add_funds_acc = 1100;
+        int bonus;
+
+        if (add_funds_acc > 1000) {
+            bonus = add_funds_acc / 100;
+            int total_sum = add_funds_acc + initial_account;
+            System.out.println("Бонусы: " + bonus);
+            System.out.println("Итогова сумма на счёту: " + (total_sum + bonus));
+        } else {
+            //int total_sum = add_funds_acc + initial_account;
+            System.out.println("Итоговая сумма " + (add_funds_acc + initial_account));
+        }
+    }
+}
